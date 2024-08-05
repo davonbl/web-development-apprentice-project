bcbb607c306a6816f3caf16595a0ae8e070f4210
davonbl-bl <davonbl@gmail.com>
2024-08-04 21:10:05 -0400

    created checkbox functionality where if a listed task is checked on, it is then cross out unless it is clicked on again

--- client/src/components/ToDoList.jsx
+++ client/src/components/ToDoList.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import "../styles/toDo.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 
-export default function ToDoList({list}){
+export default function ToDoList({list, setList}){
 
-    const clickButton = (number) => {
-        console.log(`button clicked, and number ${number}`)
+    // const clickButton = (id, completedTask) => {
+    //     console.log(`button clicked, and id ${id}`)
+    //     if(!completedTask){
+    //         setList((list) => {
+    //             list.map((ele) => {
+    //                 if(ele.id === id){
+    //                     return ele.completed = true; 
+    //                 }
+    //             })
+    //         })
+    //         console.log(`value of completed: ${completedTask}`)
+    //     }
+    // }
+    const clickButton = (id, completedTask) => {
+        console.log(`button clicked, and id ${id}`)
+        console.log('value of completed task: ', !completedTask)
+        console.log('here is the list: ', list)
+            setList((prevList) => {
+                return prevList.map((ele) => {
+                    if(ele.id === id){
+                        return {
+                            ...ele,
+                            completed: !completedTask
+                        }
+                    }else{
+                        return ele
+                    }
+                })
+            })
+            console.log(`value of completed: ${!completedTask}`)        
+    }
+
+    const deleteButton = (id) => {
+        console.log('here is the delete button')
+        console.log('here is the id of clicked object: ', id)
+        console.log('here is the list: ', list)
+        const testValue = list.filter(ele => ele.id !== id)
+        console.log('what is filtered: ', testValue)
+        // setList((currentList) => {
+        //     return currentList.filter(ele => currentList.id !== ele.id)
+        // })
+    }
+
+    const editButton = (event) => {
+        console.log('here is the edit button')
     }
     return(
         <>
@@ -12,11 +59,29 @@ export default function ToDoList({list}){
             <ol>
             {
                 list.map((ele, i) => {
-
                     return(
-                        <li key= {i + 1}>
-                            <p>{ele}</p>
-                            <button onClick={() => {return clickButton(i + 1)}}>Edit</button>
+                        // key = {i + 1}
+                        <li key= {ele.id}
+                          className="listStyles"  
+                        >
+                            <input 
+                                type="checkbox" 
+                                // name="day" 
+                                checked={ele.completed}
+                                value={ele.id} 
+                                onChange={() =>clickButton(ele.id, ele.completed)}
+                                >
+                            </input>
+                            <p className={ele.completed? "completedTask" : "pendingTask"}>{ele.toDo}</p>
+                            <FontAwesomeIcon 
+                                icon={faPenToSquare}
+                                onClick={editButton}
+                                />
+                            <FontAwesomeIcon 
+                                icon={faTrash}
+                                onClick={() => deleteButton(ele.id)}
+                                />
+                            {/* <button onClick={() => {return clickButton(i + 1)}}>Edit</button> */}
                         </li>
                     )
                 })
